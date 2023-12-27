528e62da54a900e1b079f2ebf9dbec8e1263e2eb
Caio Bruno <caiobruno90@yahoo.com>
2023-12-27 15:25:16 -0300

    Função de limpar o formulario

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,9 +1,15 @@
 import './Button.css';
 
-function Button({id, text}) {
-  return (
-    <button id={id}>{text}</button>
-  )
+function Button({ id, text, action }) {
+    const handleAction = (e) => {
+        action(e);
+    }
+
+    return (
+        <button id={id} onClick={handleAction}>
+            {text}
+        </button>
+    )
 }
 
 export default Button
--- src/components/ImcCalc.jsx
+++ src/components/ImcCalc.jsx
@@ -1,29 +1,56 @@
+import { useState } from "react";
+
 import Button from "./Button";
+
 import "./ImcCalc.css";
 
 function ImcCalc() {
-  return (
-    <div id="calc-container">
-        <h2>Calculadora de IMC</h2>
-        <form id="imc-form">
-            <div className="form-inputs">
-                <div className="form-control">
-                    <label htmlFor="height">Altura:</label>
-                    <input type="text" name="height" id="height" placeholder="Exemplo 1,75" />
+    const [height, setHeight] = useState("");
+    const [weight, setWeight] = useState("");
+
+
+   const clearForm=(e)=>{
+    e.preventDefault();
+      setHeight("")
+      setWeight("")
+   }
+
+    return (
+        <div id="calc-container">
+            <h2>Calculadora de IMC</h2>
+            <form id="imc-form">
+                <div className="form-inputs">
+                    <div className="form-control">
+                        <label htmlFor="height">Altura:</label>
+                        <input
+                            type="text"
+                            name="height"
+                            id="height"
+                            placeholder="Exemplo 1,75"
+                            onChange={(e)=>setHeight(e.target.value)}
+                            value={height}
+                        />
+                    </div>
+                    <div className="form-control">
+                        <label htmlFor="weight">Peso:</label>
+                        <input
+                        type="text" 
+                        name="weight" 
+                        id="weight"
+                         placeholder="Exemplo 70,5"
+                         onChange={(e)=>setWeight(e.target.value)}
+                         value={weight}
+                         />
+                    </div>
                 </div>
-                <div className="form-control">
-                    <label htmlFor="weight">Altura:</label>
-                    <input type="text" name="weight" id="weight" placeholder="Exemplo 70,5" />
+                <div className="action-control">
+                    <Button id="calc-btn" text="Calcular" action={clearForm} />
+                    <Button id="clear-btn" text="Limpar" />
                 </div>
-            </div>
-            <div className="action-control">
-                <Button id="calc-btn" text="Calcular"/>
-                <Button id="clear-btn" text="Limpar"/>
-            </div>
-        </form>
-        
-    </div>
-  )
+            </form>
+
+        </div>
+    )
 }
 
 export default ImcCalc
